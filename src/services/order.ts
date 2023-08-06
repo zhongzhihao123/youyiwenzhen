9af3577b5b7936a1346c0fc066031da022ad09d0
v_zhihzhong <514974922@qq.com>
2023-08-06 23:03:14 +0800

    tss

--- src/services/order.ts
+++ src/services/order.ts
@@ -16,3 +16,4 @@ export const getMedicalOrderDetail = (id: string) =>
 // 5. 订单-查询物流信息
 export const getMedicalOrderLogistics = (id: string) =>
   request<Logistics>(`/patient/order/${id}/logistics`)
+console.log(3223)
